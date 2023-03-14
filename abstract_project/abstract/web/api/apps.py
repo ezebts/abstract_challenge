2b02848f70f59d4f1fe6c490b472d38f39429a1e
ebitschin <ebitschin@folderit.net>
2023-03-14 19:13:58 -0300

    Refactor project, apps and namings for better structure

--- abstract_project/abstract/web/api/apps.py
+++ abstract_project/abstract/web/api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AbstractApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abstract.api'
+    name = 'abstract.web.api'
--- abstract_project/abstract/web/wsgi.py
+++ abstract_project/abstract/web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for abstract_backend project.
+WSGI config for Abstract project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstract_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstract.settings')
 
 application = get_wsgi_application()
